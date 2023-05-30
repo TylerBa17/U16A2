bcbfc3d604bc74efa2363b9625ef981f237b3f2e
Tyler Ball <90774947+TylerBa17@users.noreply.github.com>
2023-05-30 16:13:35 +0100

    Add files via upload

---
+++ ToDoList/ToDoList/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ToDoList
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private List<Task> tasks;
+        private bool showIncompleteOnly;
+
+        public List<Task> Tasks
+        {
+            get
+            {
+                if (showIncompleteOnly)
+                    return tasks.FindAll(task => !task.Completed);
+                else
+                    return tasks;
+            }
+        }
+        private void CompleteTask_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Task task = button.DataContext as Task;
+
+            if (task != null)
+            {
+                task.Completed = true;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Tasks"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            tasks = new List<Task>();
+            showIncompleteOnly = false;
+            DataContext = this;
+        }
+
+        private void AddTask_Click(object sender, RoutedEventArgs e)
+        {
+            string title = txtTitle.Text;
+            string description = txtDescription.Text;
+            DateTime? dueDate = dpDueDate.SelectedDate;
+
+            if (!string.IsNullOrEmpty(title) && dueDate.HasValue)
+            {
+                Task newTask = new Task(title, description, dueDate.Value);
+                tasks.Add(newTask);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Tasks"));
+                ResetInputs();
+            }
+            else
+            {
+                MessageBox.Show("Please enter title and due date.");
+            }
+        }
+
+        private void ResetInputs()
+        {
+            txtTitle.Text = string.Empty;
+            txtDescription.Text = string.Empty;
+            dpDueDate.SelectedDate = null;
+        }
+
+        private void ShowIncomplete_Checked(object sender, RoutedEventArgs e)
+        {
+            showIncompleteOnly = true;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Tasks"));
+        }
+
+        private void ShowIncomplete_Unchecked(object sender, RoutedEventArgs e)
+        {
+            showIncompleteOnly = false;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Tasks"));
+        }
+    }
+
+    public class Task
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool Completed { get; set; }
+
+        public Task(string title, string description, DateTime dueDate)
+        {
+            Title = title;
+            Description = description;
+            DueDate = dueDate;
+            Completed = false;
+        }
+    }
+}
+
